3bea7256bce1d4c09fd32db07c02a871aa0f2842
Omer Faruk Bilgin <omerfrkblgin@gmail.com>
2024-01-20 14:43:57 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    public class MusteriManager
+    {
+        private List<Musteri> musteriler;
+
+        public MusteriManager()
+        {
+            musteriler = new List<Musteri>();
+        }
+
+        public void MusteriEkle(Musteri musteri)
+        {
+            musteriler.Add(musteri);
+            Console.WriteLine($"{musteri.Ad} {musteri.Soyad} adlı müşteri eklendi.");
+        }
+
+        public void MusteriListele()
+        {
+            Console.WriteLine("Müşteri Listesi:");
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine($"ID: {musteri.Id} | Ad: {musteri.Ad} | Soyad: {musteri.Soyad}");
+            }
+        }
+
+        public void MusteriSil(int id)
+        {
+            Musteri silinecekMusteri = musteriler.Find(m => m.Id == id);
+
+            if (silinecekMusteri != null)
+            {
+                musteriler.Remove(silinecekMusteri);
+                Console.WriteLine($"{silinecekMusteri.Ad} {silinecekMusteri.Soyad} adlı müşteri silindi.");
+            }
+            else
+            {
+                Console.WriteLine("Belirtilen ID'ye sahip müşteri bulunamadı.");
+            }
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using ClassMetotDemo;
+
+class Program
+{
+    static void Main()
+    {
+        MusteriManager musteriManager = new MusteriManager();
+
+        Musteri musteri1 = new Musteri(1, "Ömer", "Bilgin", "omer@gmail.com");
+        Musteri musteri2 = new Musteri(2, "Hasan", "Tahsin", "hasan@gmail.com");
+        Musteri musteri3 = new Musteri(3, "Talat", "Paşa", "talat@gmail.com");
+        Musteri musteri4 = new Musteri(4, "Bumin", "Kağan", "bumin@gmail.com");
+
+        musteriManager.MusteriEkle(musteri1);
+        musteriManager.MusteriEkle(musteri2);
+        musteriManager.MusteriEkle(musteri3);
+        musteriManager.MusteriEkle(musteri4);
+
+        musteriManager.MusteriListele();
+
+        musteriManager.MusteriSil(1);
+
+        musteriManager.MusteriListele();
+    }
+}
